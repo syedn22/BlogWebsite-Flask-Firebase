ab56bcc0e9ab5a9fdc77652f686a72e07778ba37
syed mohammed <syedmohammedn22@gmail.com>
2020-10-21 15:16:42 +0530

    Login / SignUp added - firebase

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pyrebase
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 
 db = SQLAlchemy(app)
 
+firebaseConfig = {
+    "apiKey": "AIzaSyDv6WuyVD_A6nAc-UCrttOcHjYxf6ujQuc",
+    "authDomain": "blog-app-fbe3f.firebaseapp.com",
+    "databaseURL": "https://blog-app-fbe3f.firebaseio.com",
+    "projectId": "blog-app-fbe3f",
+    "storageBucket": "blog-app-fbe3f.appspot.com",
+    "messagingSenderId": "964400661258",
+    "appId": "1:964400661258:web:550f779ad51f30c1f3f0f3"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+auth = firebase.auth()
+
+
+class AccountDetails(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100),nullable=False)
+    username = db.Column(db.String(100),nullable=False)
+
+    def __repr__(self):
+        return 'Account ' + str(self.id)
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,14 +48,38 @@ def Home():
     return render_template('Home.html')
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['Password']
+        confirmPassword = request.form['ConfirmPassword']
+        if password == confirmPassword:
+            try:
+                result = auth.create_user_with_email_and_password(
+                    email, password)
+                return redirect("/posts")
+            except:
+                return "Failed"
+    else:
+        return render_template('signup.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['Password']
+        print(email)
+        print(password)
+        try:
+            result = auth.sign_in_with_email_and_password(email, password)
+            print(auth.current_user)
+            return redirect("/posts")
+        except:
+            return "Failed"
+    else:
+        return render_template('login.html')
 
 
 @app.route('/posts', methods=['GET', 'POST'])
@@ -58,6 +105,7 @@ def newPost():
         post_title = request.form['title']
         post_content = request.form['content']
         post_author = request.form['author']
+        print(post_title)
         new_post = BlogPost(
             title=post_title, content=post_content, author=post_author)
         db.session.add(new_post)
