6683257508d395cf3b7a5a0b67d6579d6006bfe9
syed mohammed <syedmohammedn22@gmail.com>
2020-10-21 11:57:45 +0530

    FrontEnd Edited
    1. Bootstrap-4 is added
    2. Edit page Bug Fixed
    3. SignUp/Login page included

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -7,34 +7,58 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 
 db = SQLAlchemy(app)
 
+
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    author  = db.Column(db.String(20), nullable=False, default='N/A')
-    date_posted = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
 
     def __repr__(self):
-       return 'Blog post '+ str(self.id)
+        return 'Blog post ' + str(self.id)
 
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
-
-@app.route('/posts',methods=['GET','POST'])
+@app.route('/posts', methods=['GET', 'POST'])
 def posts():
 
     if request.method == 'POST':
         post_title = request.form['title']
         post_content = request.form['content']
         post_author = request.form['author']
-        new_post = BlogPost(title=post_title,content=post_content, author=post_author)
+        new_post = BlogPost(
+            title=post_title, content=post_content, author=post_author)
         db.session.add(new_post)
         db.session.commit()
         return redirect('/posts')
     else:
         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
         return render_template('posts.html', posts=all_posts)
+
+
+@app.route('/newPost')
+def newPost():
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(
+            title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('newPost.html')
 
 
 @app.route('/posts/delete/<int:id>')
@@ -45,21 +69,19 @@ def delete(id):
     return redirect('/posts')
 
 
-@app.route('/posts/edit/<int:id>',methods=['GET','POST'])
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
 
     post = BlogPost.query.get_or_404(id)
-    if request.method ==  'POST':
+    if request.method == 'POST':
         post.title = request.form['title']
         post.author = request.form['author']
         post.content = request.form['content']
         db.session.commit()
         return redirect('/posts')
     else:
-        return render_template('edit.html',post=post)
-
-
+        return render_template('edit.html', post=post)
 
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
